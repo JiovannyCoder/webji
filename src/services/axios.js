52e3ebb3f91b4efd6e48c8d3fe7592889f6064f8
JiovannyCoder <haritina110602@gmail.com>
2023-02-15 18:44:00 +0300

    Headers authorization rectified

--- src/services/axios.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios"
-const token = process.env.VUE_APP_API_KEY
-axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-
-export default axios
--- src/services/categoriesServices.js
+++ src/services/categoriesServices.js
@@ -1,4 +1,6 @@
-import axios from "./axios"
+import axios from "axios"
+const token = process.env.VUE_APP_API_KEY
+axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
 
 const API_URL = process.env.VUE_APP_API_URL
 
